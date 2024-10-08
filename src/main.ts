cf8b19334862c896cdbadf938fc0da24e4faee94
minjae0402 <kangjack0402@gmail.com>
2024-10-08 14:59:26 +0900

    hoooo

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,7 @@ import { setupCounter } from './counter.ts';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <h1>이은교 특검 2222222</h1></h1>
-    <h1>강민재 행정실 쿠데타</h1></h1>
-    <p>199석 강민재 특검하라</p>
+    <h1>hooooooooooo</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
