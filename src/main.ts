a0ea54b4585bfd5fee329935d70692fa36e6f603
minjae0402 <kangjack0402@gmail.com>
2024-10-08 14:28:11 +0900

    feat: add babo

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { setupCounter } from './counter.ts';
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1>강민재 행정실 쿠데타</h1></h1>
+    <p>바보</p>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
